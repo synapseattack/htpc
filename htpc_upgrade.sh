a2ecf87af123445cdc03eccac643cf4d980ff840
Brad Nestelroad <mad.dba@gmail.com>
2017-04-26 19:53:42 -0600

    add color to the echo for each step
    For all echo commands the -e flag was added to support the color flags
    \e[32m = red
    \e[31m = gree
    \e[0m  = stops fomatting

--- htpc_upgrade.sh
+++ htpc_upgrade.sh
@@ -3,39 +3,39 @@
 export docker_compose_dir="/docker/htpc"
 export docker_backup_dir="/fileserver/docker_backup"
 
-#echo "* Pause the sabnzbd queue"
+#echo -e "* \e[32mPause the sabnzbd queue\e[0m"
 # TODO: curl command not currently working
 # curl -k https://localhost:9090/sabnzbd/api?apikey=81c20d47846364b33dd232f1330b6be4&output=json&mode=queue
 
-#echo "* Check for active Plex users"
+#echo -e "* \e[32mCheck for active Plex users\e[0m"
 
-echo "* Stop all HTPC docker containers"
+echo -e "* \e[32mStop all HTPC docker containers\e[0m"
 cd $docker_compose_dir
 docker-compose down
 
-#echo "* Backup all HTPC docker volumes"
+#echo -e "* \e[32mBackup all HTPC docker volumes\e[0m"
 # TODO: What directories can be excluded during tar.
 #       Check plex for backup recommendations
 #sudo -u htpc tar -cvzf $docker_backup_dir/$(date +%Y%m%d%H%M%S)_htpc.tar.gz /docker
 
-echo "* Pull the latest version of all HTPC images"
+echo -e "* \e[32mPull the latest version of all HTPC images\e[0m"
 for image in $(docker images | grep linuxserver |awk '{ print $1 }'); 
 do
 	docker pull $image; 
 done;
 
-#echo "* Git pull new docker-compose file"
+#echo -e "* \e[32mGit pull new docker-compose file\e[0m"
 
 
-echo "* Restart HTPC docker containers"
+echo -e "* \e[32mRestart HTPC docker containers\e[0m"
 cd $docker_compose_dir
 docker-compose up -d
 
-echo "* Clean up old docker containers"
+echo -e "* \e[32mClean up old docker containers\e[0m"
 if [ $(docker images |grep "<none>" | wc -l) -gt 0 ]
 then
 	docker rmi $(docker images |grep "<none>" |awk '{print $3}')
 else
-	echo "  - No linuxserver images to clean up"
+	echo -e "  - \e[31mNo linuxserver images to clean up\e[0m"
 fi
 
